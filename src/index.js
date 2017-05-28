2f7f0885396ed263b9091fe9012cbecea1c62038
tsvetkovpro <285019@gmail.com>
2017-05-28 08:32:45 +0500

    manipulating data via json server

--- src/index.js
+++ src/index.js
@@ -17,4 +17,18 @@ getUsers().then(result => {
 	});
 
 	global.document.getElementById('users').innerHTML = usersBody;
+
+	const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+	// Must use array.from to create a real array from a DOM collection
+	// getElementsByClassname only returns an "array like" object
+	Array.from(deleteLinks, link => {
+		link.onclick = function(event) {
+			const element = event.target;
+			event.preventDefault();
+			deleteUser(element.attributes["data-id"].value);
+			const row = element.parentNode.parentNode;
+			row.parentNode.removeChild(row);
+		};
+	});
 });
